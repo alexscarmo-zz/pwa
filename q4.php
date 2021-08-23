ea5151b77a33ae2f462bc36af29f53f2171006b0
Alex <sc_tecnologia@hotmail.com>
2021-08-22 22:40:40 -0300

    Alterações q4

--- q4.php
+++ q4.php
@@ -23,8 +23,24 @@
         private $turmas = array();
         private $nmax;
 
+        public function __construct($nmax){
+            $this->nmax = $nmax;
+        }
+
         public imprimeTurmas();
 
         public calculaTurmas($disciplina);
     }
+
+    $aluno1 = new Aluno("João", 8.5);
+    $aluno2 = new Aluno("Ana", 9.0);
+    $aluno3 = new Aluno("Maria", 9.5);
+
+    $disciplina1 = new Disciplina("PAW");
+
+    $turmasDisciplina = new TurmasDisciplina(2);
+
+    $aluno1->inscreveDisciplina($disciplina1);
+    $aluno2->inscreveDisciplina($disciplina1);
+    $aluno3->inscreveDisciplina($disciplina1);
 ?>
